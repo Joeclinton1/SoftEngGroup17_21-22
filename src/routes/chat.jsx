8f769cb1f32b9192b9888ff0453e7cf10f81140c
Joel Palmer <joeljpalmer@btinternet.com>
2022-02-17 12:20:20 +0000

    Query discovery

--- src/routes/chat.jsx
+++ src/routes/chat.jsx
@@ -71,6 +71,33 @@ class Chat extends Component{
     }
 
     handleSend = (text) => {
+        console.log(text)
+        const DiscoveryV1 = require('ibm-watson/discovery/v1');
+        const { IamAuthenticator } = require('ibm-watson/auth');
+
+        const discovery = new DiscoveryV1({
+        version: '2019-04-30',
+        authenticator: new IamAuthenticator({
+            apikey: '__Kp8R3pLrESr2vmVT4vRjsgBEo5ZKkWHPPar0pOOti2',
+        }),
+        serviceUrl: 'https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/d874b546-9b02-4c6a-bc6c-3042fedb37be',
+        });
+
+        const queryParams = {
+        environmentId: '8b58da18-58c8-49eb-b5d4-4cbc7d7f58fa',
+        collectionId: '2e651944-431c-4dbc-b407-716036caea75',
+        configurationId: '90941570-b5c8-4d55-b39a-cd9b26cdf9a8',
+        Query: text
+        };
+
+        discovery.query(queryParams)
+        .then(queryResponse => {
+            console.log(JSON.stringify(queryResponse, null, 2));
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+
         const {currentMessages} = this.state
         const id = currentMessages.length;
         const msg = new ChatMessage(id, text);
