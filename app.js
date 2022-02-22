a98eaa95ae32a49a9f328311bf78123acb57d723
Rei Ishii <50512871+re8423@users.noreply.github.com>
2022-02-22 17:00:21 +0000

    Moved logic to backend (Moved relevancy code)

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ authenticator: new IamAuthenticator({
 serviceUrl: 'https://api.eu-gb.discovery.watson.cloud.ibm.com/instances/d874b546-9b02-4c6a-bc6c-3042fedb37be',
 });
 
+//Relevancy reqeust
+app.get('/relev', (req,res) =>{
+    var text = String(req.query.rtext)
+    var split_text = text.split("^")
+    var resultST = split_text[0]
+    var resultDI = split_text[1]
+
+    const createEventParams = {
+        type: 'click',
+        data: {
+          environment_id: '8b58da18-58c8-49eb-b5d4-4cbc7d7f58fa',
+          session_token: resultST,
+          collection_id: '2e651944-431c-4dbc-b407-716036caea75',
+          document_id: resultDI,
+        }
+      };
+
+    discovery.createEvent(createEventParams)
+        .then(createEventResponse => {
+        //   console.log(JSON.stringify(createEventResponse, null, 2));
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.setHeader("Content-Type", "application/json");
+        res.send(JSON.stringify(createEventResponse, null, 2))
+        })
+        .catch(err => {
+          console.log('error:', err);
+        });
+});
 
 app.get('/queryWD', (req, res) => {
 
