e8e940ad18daba3827a135025cf1769afe05c2d5
Joe Clinton <joeclinton1@btinternet.com>
2022-02-25 15:51:39 +0000

    prevent drawer width prop being forwarded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
-import {useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
-const Title = () =>{
-    switch(useLocation().pathname){
+const Title = () => {
+    switch (useLocation().pathname) {
         case "/":
             return "Chat";
         case "/options":
@@ -17,41 +17,45 @@ const Title = () =>{
         default:
             return "";
     }
-    
+
 }
 
 const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-  })(({ theme, open, drawerWidth }) => ({
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: `${drawerWidth}px`,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-}));
+    shouldForwardProp: prop => (
+        prop !== 'open' && 
+        prop !== 'drawerWidth'
+    ),
+})(
+    ({ theme, open, drawerWidth }) => ({
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        ...(open && {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: `${drawerWidth}px`,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        }),
+    }));
 
-export default function NavBar(props){
-    return(
+export default function NavBar(props) {
+    return (
         <AppBar position="fixed" drawerWidth={props.drawerWidth} open={props.open}>
             <Toolbar>
                 <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={props.handleDrawerOpen}
-                edge="start"
-                sx={{ mr: 2, ...(props.open && { display: 'none' }) }}
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={props.handleDrawerOpen}
+                    edge="start"
+                    sx={{ mr: 2, ...(props.open && { display: 'none' }) }}
                 >
-                <MenuIcon />
+                    <MenuIcon />
                 </IconButton>
-                <Typography variant="h4" noWrap  component="div" align = "center" sx={{ flexGrow: 1 }}>
-                    <Title/>
+                <Typography variant="h4" noWrap component="div" align="center" sx={{ flexGrow: 1 }}>
+                    <Title />
                 </Typography>
             </Toolbar>
         </AppBar>
