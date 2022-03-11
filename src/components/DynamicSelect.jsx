f103a4085e5987d9d4d09feb69f5a2861bc498c4
Joe Clinton <joeclinton1@btinternet.com>
2022-03-11 14:23:36 +0000

    hotfix DynamicSelect

--- src/components/DynamicSelect.jsx
+++ src/components/DynamicSelect.jsx
@@ -2,8 +2,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
-export default function DynamicSelect ({id, label, state, changeCookie, handleChange, MenuItems}){
+const DynamicSelect = ({id, label, state, changeCookie, handleChange, MenuItems}) => (
     /*
     id: a unique name to describe the form input
     label: the human readable name that will be displayed
@@ -29,4 +28,6 @@ export default function DynamicSelect ({id, label, state, changeCookie, handleCh
         ))}
         </Select>
     </FormControl>
-}
+)
+
+export default DynamicSelect;
